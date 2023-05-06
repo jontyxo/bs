0125d05d65f5e2e152cb5f0bef41dafd9b214b03
jontyxo <joyesh16wa@gmail.com>
2023-05-06 21:45:12 +0530

    s

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+ 
+  
+
+    <div class="card">
+    <div class="back"></div>
+    <div class="front">
+      <div class="imgset">
+           <img width="100%" src="https://1.bp.blogspot.com/-Mgj9-rbs65E/XfMoPSD5gtI/AAAAAAAAURk/NBokE2gSS2cTSJ2em5lZ5hJDuTtRN7UVwCLcBGAsYHQ/s1600/2713997.png" />
+         </div>
+    </div>
+    <div class="text-container">
+      <p id="head">Happy Birthday Sonal♥</p>
+      <p>I hope your special day will bring you lots of happiness, love, and fun. You deserve them a lot. Enjoy!</p>
+      <p>Hope this year will be great for you!</p>
+    </div>
+  </div>
+  );
+}
+
+export default App;
